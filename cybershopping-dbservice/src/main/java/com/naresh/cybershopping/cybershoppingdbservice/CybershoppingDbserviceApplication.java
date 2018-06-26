a5a8e082ac4821e056e29f90db22b09f23614f88
nares <nares@172.16.123.128>
2018-06-26 14:14:24 -0700

    some changes

---
+++ cybershopping-dbservice/src/main/java/com/naresh/cybershopping/cybershoppingdbservice/CybershoppingDbserviceApplication.java
@@ -0,0 +1,12 @@
+package com.naresh.cybershopping.cybershoppingdbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybershoppingDbserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CybershoppingDbserviceApplication.class, args);
+	}
+}
